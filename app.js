c1dd38d8fcc6746bb193817d61b7be5b2d566dde
Nicholas Nooch <nnlaor@gmail.com>
2017-01-29 11:29:24 -0800

    Detailing the package.json + adding bootstrap sample

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-
 var express = require('express');
 
-var hi = "Hello"
-console.log(hi)
+var app = express();
+
+var port = 5000;
+
+// requests to server for static files will be searched here by express
+app.use(express.static('public'));
+
+//Routes are searched after above
+app.get('/', function(req, res){
+	res.send("hello world");
+});
+
+app.get('/books', function(req, res){
+	res.send("hello books");
+});
+
+app.listen(port, function(err){
+	console.log('running server on port '  +  port);
+});
+
