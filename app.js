f932b987c76272d1840849692d6c0573745bdb3d
Nicholas N <nnlaor@gmail.com>
2017-05-13 21:26:41 -0700

    book route

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ var port = process.env.PORT || 5000;
 // //Routes are searched after static
 
 app.get('/',function(req, res){
-	res.send('Hello Books');
+	// req is from a post
+	// res gets sent back to user
+	res.send('Hello World');
 });
+
+app.get('/books',function(req, res){
+	res.send('Hello BOOKS');
+});
+
 // // requests to server for static files will be searched here by express
 // express server will server this up, even if this is a static file
 app.use(express.static('public'));
